98371c86f089c41bec82455da1b3b3f1d23f2160
AlessiaLuongo <151174462+AlessiaLuongo@users.noreply.github.com>
2024-05-27 23:00:46 +0200

    setting FE url allowed origin

--- src/main/java/alessia/security/Config.java
+++ src/main/java/alessia/security/Config.java
@@ -48,7 +48,7 @@ public class Config {
     @Bean
     CorsConfigurationSource corsConfigurationSource() {
         CorsConfiguration configuration = new CorsConfiguration();
-        configuration.setAllowedOrigins(Arrays.asList("http://localhost:5173", "https://capstone-frontend-kappa.vercel.app"));
+        configuration.setAllowedOrigins(Arrays.asList("http://localhost:5173", "https://capstone-frontend-2zbsq8b4y-alessials-projects.vercel.app"));
         configuration.setAllowedMethods(Arrays.asList("*"));
         configuration.setAllowedHeaders(Arrays.asList("*"));
         UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
