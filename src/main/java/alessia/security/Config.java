f7ca87d50c35bf1caf70aaf83b14d86d88a40241
AlessiaLuongo <151174462+AlessiaLuongo@users.noreply.github.com>
2024-05-27 15:02:36 +0200

    config vercel

--- src/main/java/alessia/security/Config.java
+++ src/main/java/alessia/security/Config.java
@@ -48,7 +48,7 @@ public class Config {
     @Bean
     CorsConfigurationSource corsConfigurationSource() {
         CorsConfiguration configuration = new CorsConfiguration();
-        configuration.setAllowedOrigins(Arrays.asList("http://localhost:5173"));
+        configuration.setAllowedOrigins(Arrays.asList("http://localhost:5173", "https://capstone-frontend-rho.vercel.app"));
         configuration.setAllowedMethods(Arrays.asList("*"));
         configuration.setAllowedHeaders(Arrays.asList("*"));
         UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
